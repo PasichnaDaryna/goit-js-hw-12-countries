de3896c41e2e93c4ecf0388805361b96f00d52a7
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2020-11-02 13:10:00 +0200

    corrections

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
-import API from "./api-service";
 import countryTpl from "./templates/country-card.hbs";
+import API from "./api-service";
 import getRefs from "./get-refs";
-// import debounce from "lodash.debounce";
 const refs = getRefs();
-refs.searchForm.addEventListener('submit', onSearch)
-
+refs.searchForm.addEventListener('input', onSearch)
 function onSearch(e) {
   e.preventDefault();
-
   const form = e.currentTarget;
-  console.log(e.currentTarget)
   const searchQuery = form.elements.query.value;
    API.fetchCountry(searchQuery)
-    .then(renderCountryCard)
-    .catch(onFetchError)
-    .finally(() => form.reset());
+    .then(renderCountryCard).catch(onFetchError).finally(() => form.reset());
 };
-
 function renderCountryCard(country) {
    const markup = countryTpl(country[0]);
   refs.cardContainer.innerHTML = markup;
 };
 function onFetchError(error) {
-  alert('Hello! I am an alert box!!Nothing was found!');
-  
+  return alert('Hello! I am an alert box!!Nothing was found!');
 }
-
